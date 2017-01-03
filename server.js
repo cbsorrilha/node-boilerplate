8d1bdfe1b5b19310810bbdfe57c260ad9e91a590
Cesar de Barros <cbsorrilha@hotmail.com>
2017-01-03 16:02:37 -0200

    Mongodb implementado

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+const MongoClient = require('mongodb').MongoClient
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+const app = express()
+
+MongoClient.connect('mongodb://localhost:27017/starwars-quotes', (err, database) => {
+    if (err) return console.log(err)
+    db = database
+    // ... start the server
+    app.get('/', (req, res) => {
+        var cursor =  db.collection('quotes').find().toArray(function(err, results) {
+            console.log(results)
+            // send HTML file populated with quotes here
+             res.send('hello world')
+        })
+    })
+
 })
+
 
 app.listen(4000)
